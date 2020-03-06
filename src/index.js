90ed4559882360ec3796bf1a28d74f18bd5b0c1b
KirillZh <kirill.zhdanov97@gmail.com>
2020-03-06 22:22:33 +0300

    feat:Solution added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for(let i=0;i<str.length;i++){
+      for(j=0;j<bracketsConfig.length;j++){
+        let tmp="";
+        tmp=bracketsConfig[j][0]+""+bracketsConfig[j][1];
+      if(str.includes(tmp)){
+        str=str.replace(tmp,"");
+        i=0;
+        j=0;
+      }
+    }
+  }
+  return str.length==0?true:false;
 }
